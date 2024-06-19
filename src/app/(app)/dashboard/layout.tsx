903f7acee3d5a86af9a84b750fa04c98f938b28f
Kapil Paliwal <kapilsuham@gmail.com>
2024-06-19 15:51:28 +0530

    updated dashboard layout.ts

---
+++ src/app/(app)/dashboard/layout.tsx
@@ -0,0 +1,19 @@
+import Navbar from '@/components/Navbar';
+import { getServerSession } from 'next-auth';
+import { authOptions } from '../../api/auth/[...nextauth]/options';
+import { redirect } from 'next/navigation';
+interface RootLayoutProps {
+  children: React.ReactNode;
+}
+
+export default async function RootLayout({ children }: RootLayoutProps) {
+  const session =await getServerSession(authOptions)
+  if (!session || !session.user) {
+      redirect('/sign-in')
+  }
+  return (
+    <div className="flex flex-col min-h-screen bg-base-200">
+      {children}
+    </div>
+  );
+}
