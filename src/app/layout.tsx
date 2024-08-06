ce58b748553989979cb6e1cf164a8087a7c4f524
Kapil Paliwal <kapilsuham@gmail.com>
2024-08-06 18:27:40 +0530

    update insight

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -5,6 +5,8 @@ import { Toaster } from 'sonner';
 import AuthProvider from "@/(more)/context/AuthProvider";
 import PlausibleProvider from "next-plausible";
 import { Analytics } from "@vercel/analytics/react"
+import { SpeedInsights } from "@vercel/speed-insights/next"
+
 const inter = Inter({ subsets: ["latin"] });
 export const metadata: Metadata = {
   metadataBase: new URL(process.env.NEXTAUTH_URL as string),
@@ -40,6 +42,7 @@ export default function RootLayout({
           </body>
         </AuthProvider>
         <Analytics/>
+        <SpeedInsights/>
       </html>
     </>
   );
